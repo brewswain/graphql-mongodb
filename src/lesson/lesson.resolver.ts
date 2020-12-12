57166d224a29f6a57635bab0b552c7cca755ed26
Brandon Lee <brandonlee2543@hotmail.com>
2020-12-12 08:35:02 -0400

    created query to retrieve all lessons in database

--- src/lesson/lesson.resolver.ts
+++ src/lesson/lesson.resolver.ts
@@ -1,4 +1,5 @@
 import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
+import { Lesson } from './lesson.entity';
 import { CreateLessonInput } from './lesson.input';
 import { LessonService } from './lesson.service';
 import { LessonType } from './lesson.type';
@@ -9,6 +10,11 @@ export class LessonResolver {
   @Query((returns) => LessonType)
   lesson(@Args('id') id: string) {
     return this.lessonService.getLesson(id);
+  }
+
+  @Query((returns) => [LessonType])
+  getAllLessons() {
+    return this.lessonService.getAllLessons();
   }
 
   @Mutation((returns) => LessonType)
